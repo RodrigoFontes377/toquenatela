feedd2134070494ec5b8235eda8e9ab1582affd1
RodrigoFontes377 <rodrigofontes377@gmail.com>
2025-01-30 03:15:43 -0300

    teste final

--- screens/InstructionsScreen.js
+++ screens/InstructionsScreen.js
@@ -4,19 +4,21 @@ import { View, Text, StyleSheet, ImageBackground } from "react-native";
 export default function InstructionsScreen() {
   return (
     <ImageBackground
-      source={require("../assets/WhatsApp Image 2025-01-29 at 15.34.38.jpeg")}
+      source={require("../assets/WhatsApp Image 2025-01-28 at 16.59.35.jpeg")}
       style={styles.background}
     >
-      <View style={styles.container}>
-        <Text style={styles.title}>Instruções do Jogo</Text>
-        <Text style={styles.instructions}>
-          1. Digite seu nome na tela de jogo.{"\n\n"}
-          2. Pressione "Iniciar" e aguarde o sinal.{"\n\n"}
-          3. Quando o sinal aparecer, toque na tela o mais rápido possível.
-          {"\n\n"}
-          4. Seu tempo de reação será registrado e salvo.{"\n\n"}
-          5. Confira seu desempenho na tela de ranking.
-        </Text>
+      <View style={styles.overlay}>
+        <View style={styles.container}>
+          <Text style={styles.title}>Instruções do Jogo</Text>
+          <Text style={styles.instructions}>
+            1. Digite seu nome na tela de jogo.{"\n\n"}
+            2. Pressione "Iniciar" e aguarde o sinal.{"\n\n"}
+            3. Quando o sinal aparecer, toque na tela o mais rápido possível.
+            {"\n\n"}
+            4. Seu tempo de reação será registrado e salvo.{"\n\n"}
+            5. Confira seu desempenho na tela de ranking.
+          </Text>
+        </View>
       </View>
     </ImageBackground>
   );
@@ -24,11 +26,17 @@ export default function InstructionsScreen() {
 
 const styles = StyleSheet.create({
   background: { flex: 1 },
-  container: {
+  overlay: {
     flex: 1,
-    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
     justifyContent: "center",
+    alignItems: "center",
+  },
+  container: {
     padding: 20,
+    backgroundColor: "rgba(0, 0, 0, 0.6)",
+    borderRadius: 10,
+    alignItems: "center",
   },
   title: {
     fontSize: 24,
@@ -36,6 +44,7 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     color: "white",
     letterSpacing: 1.5,
+    textAlign: "center",
   },
   instructions: {
     fontSize: 18,
